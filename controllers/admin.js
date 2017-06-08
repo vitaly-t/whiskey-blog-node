2bc1cc6ea3acc27e195df906cae13a7b887d6ecb
tjgavlick <tjgavlick@gmail.com>
2017-06-08 07:46:12 -0700

    List admin's recent items on admin landing

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,15 +1,46 @@
 const express = require('express'),
       router = express.Router(),
+      auth = require('../middleware/auth'),
       User = require('../models/user/user'),
-      auth = require('../middleware/auth');
+      Review = require('../models/review/review'),
+      Post = require('../models/post/post');
 
 
 // admin landing page, showing actions and user's items
 router.get('/', auth.requireSession, function (req, res, next) {
+  let userReviews = [],
+      loggedInUser;
   User.get(req.session.userId)
     .then(user => {
-      res.send('Welcome ' + user.name);
-    });
+      loggedInUser = user;
+      return Review.list({
+        limit: 5,
+        filters: [{ field: 'author', value: user.id }]
+      });
+    })
+    .then(reviews => {
+      userReviews = reviews;
+      return Post.list({
+        limit: 5,
+        filters: [{ field: 'author', value: loggedInUser.id }]
+      });
+    })
+    .then(posts => {
+      const reviewList = userReviews.map(item => `<li><a href="/reviews/${item.slug}">${item.title}</a></li>`),
+            postList = posts.map(item => `<li><a href="/posts/${item.slug}">${item.title}</a></li>`);
+      return res.send(`
+        <h1>Welcome, ${loggedInUser.name}</h1>
+        <h2>Your recent reviews</h2>
+        <ul>
+          ${reviewList.join('')}
+        </ul>
+        <h2>Your recent posts</h2>
+        <ul>
+          ${postList.join('')}
+        </ul>
+      `);
+    })
+    .catch(next);
 });
 
 module.exports = router;
