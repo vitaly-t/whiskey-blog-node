91e781ee6b4eb21e95128ab504cb2ac258aaf7ae
Tim Gavlick <tjgavlick@gmail.com>
2017-06-15 22:51:29 -0700

    Begin a reviews list view

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,12 +1,28 @@
 const express = require('express'),
       router = express.Router(),
-      Review = require('../models/review/review');
+      Review = require('../models/review/review'),
+      DrinkType = require('../models/drink-type/drink-type'),
+      Rarity = require('../models/rarity/rarity'),
+      Region = require('../models/region/region');
 
 // return 100 recent reviews
 router.get('/', function (req, res, next) {
-  Review.list()
-    .then(reviews => res.json(reviews))
-    .catch(e => next());
+  console.log(req.query);
+  Promise.all([
+      Review.list(),
+      DrinkType.list(),
+      Rarity.list(),
+      Region.list()
+    ])
+    .then(data => {
+      return res.render('../views/reviews/list.twig', {
+        reviews: data[0],
+        drinkTypes: data[1],
+        rarities: data[2],
+        regions: data[3]
+      });
+    })
+    .catch(next);
 });
 
 // get a review by url slug
